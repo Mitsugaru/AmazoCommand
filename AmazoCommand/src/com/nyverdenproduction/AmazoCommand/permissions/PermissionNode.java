5812117a5b172317db4d92ad39edac31c828795a
Vincent Deloso <mitsugaru@gmail.com>
2012-06-14 22:06:21 -0400

    Removed extra permission node.

--- AmazoCommand/src/com/nyverdenproduction/AmazoCommand/permissions/PermissionNode.java
+++ AmazoCommand/src/com/nyverdenproduction/AmazoCommand/permissions/PermissionNode.java
@@ -2,7 +2,7 @@ package com.nyverdenproduction.AmazoCommand.permissions;
 
 public enum PermissionNode
 {
-	ADMIN_RELOAD(".admin.reload"), ADMIN_RESET(".admin.reset"), EXEMPT(".exempt");
+	ADMIN_RELOAD(".admin.reload"), EXEMPT(".exempt");
 	private static final String prefix = "AmazoCommand";
 	private String node;
 
